40b791cf6ca63f9d0e13ea0cf981bbf67798d0ea
Wonmi <wonmadesign@gmail.com>
2019-04-19 01:56:01 +0900

    Completed refactoring edit and fixed a bug for delete functionality

--- src/scripts/edit.js
+++ src/scripts/edit.js
@@ -1,38 +1,13 @@
 import uuidv4 from 'uuid/v4'
 
 const recipeID = location.hash.substring(1)
-
-let theIngres;
-let theRecipe;
-
 const token = localStorage.getItem('x-auth')
 
+let theIngres;
 let pageMode = 'createMode'
+if (recipeID.length > 0) { pageMode = 'editMode' }
 
-if (recipeID.length > 0) {
-    pageMode = 'editMode'
-}
-
-const getTypeDOM = (typeName) => {
-    // Create a child of '<select>'
-    const option = document.createElement('option')
-    option.setAttribute('id', typeName)
-    option.setAttribute('value', typeName)
-    option.classList.add('foodType')
-    option.textContent = typeName.charAt(0).toUpperCase() + typeName.slice(1).split('-').join(' ')
-
-    return option
-}
-
-const renderType = () => {
-    const filterTypes = JSON.parse(localStorage.getItem('filterTypes'))
-    const editType = document.querySelector('#editType') // === <select>
-    editType.innerHTML = ''
-    filterTypes.forEach((type) => {
-        return editType.appendChild(getTypeDOM(type.name))
-    })
-}
-
+//---------------------  Error message for user   -----------------------//
 function handleError(text) {
     const errorBox = document.querySelector('#edit__error-msg')
     const errorText = document.createElement('p')
@@ -41,67 +16,67 @@ function handleError(text) {
     errorBox.appendChild(errorText)
 }
 
-// Initiating 'Main Ingre' (DOM)
+// Make error box disappear when any part of the form is clicked.
+document.querySelector('#edit-form').addEventListener('click', () => {
+    const errorBox = document.querySelector('#edit__error-msg')
+    errorBox.innerHTML = ''
+})
+
+//-----------------------   Rendering Ingredients   -----------------------//
+// Triggered by input handler
+function updateLocalStorage(type) {
+    type === 'main' ? theIngres.mainIngre = [] : theIngres.subIngre = []
+    Array.from(document.querySelector(`#${type}IngreArea`).children).forEach((each) => {
+        console.log(each)
+
+        const _id = each.dataset.id
+        const name = each.children[0].value
+        const amount = each.children[1].value
+        type === 'main'
+            ? theIngres.mainIngre.push({ _id, name, amount })
+            : theIngres.subIngre.push({ _id, name, amount })
+    })
+    localStorage.setItem(`${type}Ingre`, JSON.stringify(theIngres[`${type}Ingre`]))
+}
+
 const generateIngreDOM = (ingre, type) => {
 
     const ingreLi = document.createElement('li')
     const nameEl = document.createElement('input')
     const amountEl = document.createElement('input')
     const removeEl = document.createElement('button')
-
+    
     nameEl.value = ingre.name.toLowerCase()
     amountEl.value = ingre.amount.toLowerCase()
+    
+    ingreLi.setAttribute('id', `${type}Ingre`)
+    ingreLi.setAttribute('data-id', ingre._id)
 
-    if(type === 'main') {
-        ingreLi.setAttribute('id', 'mainIngre')
-    } else {
-        ingreLi.setAttribute('id', 'subIngre')
-    }
     nameEl.setAttribute('placeholder', 'name')
     nameEl.setAttribute('maxlength', '15')
-    amountEl.setAttribute('placeholder', 'amount')
-    nameEl.setAttribute('maxlength', '15')
     
+    amountEl.setAttribute('placeholder', 'amount')
+    amountEl.setAttribute('maxlength', '15')
+
     // Input(name) Event Handler
-    nameEl.addEventListener('input', () => {
-        type === 'main' ? theIngres.mainIngre = [] : theIngres.subIngre = []
-        Array.from(document.querySelector(`#${type}IngreArea`).children).forEach((each) => {
-            const name = each.children[0].value
-            const amount = each.children[1].value
-            type === 'main' ? theIngres.mainIngre.push({name, amount})
-                : theIngres.subIngre.push({ name, amount })
-        })
-        localStorage.setItem(`${type}Ingre`, JSON.stringify(theIngres[`${type}Ingre`]))
+    nameEl.addEventListener('input', function () {
+        updateLocalStorage(type)
     })
 
     // Input(amount) Event Handler
-    amountEl.addEventListener('input', () => {
-        type === 'main' ? theIngres.mainIngre = [] : theIngres.subIngre = []
-        Array.from(document.querySelector(`#${type}IngreArea`).children).forEach((each) => {
-            const name = each.children[0].value
-            const amount = each.children[1].value
-            type === 'main' ? theIngres.mainIngre.push({ name, amount })
-                : theIngres.subIngre.push({ name, amount })
-        })
-        localStorage.setItem(`${type}Ingre`, JSON.stringify(theIngres[`${type}Ingre`]))
+    amountEl.addEventListener('input', function () {
+        updateLocalStorage(type)
     })
 
     // Remove functionality
-    if (type === 'main') {
-        removeEl.addEventListener('click', () => {
-            const ingreIndex = theIngres.mainIngre.findIndex((each) => each.id === ingre.id)
-            theIngres.mainIngre.splice(ingreIndex, 1)
-
-            renderIngre('main')
-        })
-    } else if (type === 'sub') {
-        removeEl.addEventListener('click', () => {
-            const ingreIndex = theIngres.subIngre.findIndex((each) => each.id === ingre.id)
-            theIngres.subIngre.splice(ingreIndex, 1)
-
-            renderIngre('sub')
-        })
-    }
+    removeEl.addEventListener('click', function () {
+        let eachIngre = JSON.parse(localStorage.getItem(`${type}Ingre`))
+        const ingreIndex = eachIngre.findIndex((each) => each._id === ingre._id)
+        eachIngre.splice(ingreIndex, 1)
+        type === 'main' ? theIngres.mainIngre = eachIngre : theIngres.subIngre = eachIngre
+        localStorage.setItem(`${type}Ingre`, JSON.stringify(theIngres[`${type}Ingre`]))
+        renderIngre(type)
+    })
 
     ingreLi.classList.add('edit__ingre')
     nameEl.classList.add('edit__ingre-name')
@@ -113,26 +88,34 @@ const generateIngreDOM = (ingre, type) => {
     return ingreLi
 }
 
-// Initiating 'Main Ingre' (Render)
 const renderIngre = (type) => {
-
     const ingreArea = document.querySelector(`#${type}IngreArea`)
     ingreArea.innerHTML = ''
-    // theIngres.mainIngre : theIngres.subIngre
     let ingreArray = type === 'main' ? theIngres.mainIngre : theIngres.subIngre
-
-    ingreArray.forEach((obj) => {
-        ingreArea.appendChild(generateIngreDOM(obj, type))
-    })
-
-    type === 'main' ? localStorage.setItem(`${type}Ingre`, JSON.stringify(theIngres.mainIngre))
-        : localStorage.setItem(`${type}Ingre`, JSON.stringify(theIngres.subIngre))
+    ingreArray.forEach(obj => ingreArea.appendChild(generateIngreDOM(obj, type)))
 }
 
-// fetch GET해서 type 정보 받아오기
+//-----------------------   Rendering Type   -----------------------//
+const getTypeDOM = (typeName) => {
+    // Create a child of '<select>'
+    const option = document.createElement('option')
+    option.setAttribute('id', typeName)
+    option.setAttribute('value', typeName)
+    option.classList.add('foodType')
+    option.textContent = typeName.charAt(0).toUpperCase() + typeName.slice(1).split('-').join(' ')
+    return option
+}
+
+const renderType = () => {
+    const filterTypes = JSON.parse(localStorage.getItem('filterTypes'))
+    const editType = document.querySelector('#editType') // === <select>
+    editType.innerHTML = ''
+    filterTypes.forEach(type => editType.appendChild(getTypeDOM(type.name)))
+}
+
 renderType()
 
-// Initial query from data
+//-----------------------  Populating input fields   -----------------------//
 if (recipeID.length > 0) { // hash 있음 (기존 Recipe 편집)
 
     fetch(`http://localhost:3000/recipes/${recipeID}`, {
@@ -144,28 +127,16 @@ if (recipeID.length > 0) { // hash 있음 (기존 Recipe 편집)
     })
         .then(response => response.json())
         .then((res) => {
-            localStorage.setItem('mainIngre', JSON.stringify(res.recipe.mainIngre))
-            localStorage.setItem('subIngre', JSON.stringify(res.recipe.subIngre))
-
-            theIngres = {
-                mainIngre: res.recipe.mainIngre,
-                subIngre: res.recipe.subIngre
-            }
-
-            renderIngre('main')
-            renderIngre('sub')
-
-            // Initiating title, type, serving, text body
-            theRecipe = res.recipe 
+            const {mainIngre, subIngre, title, type, serving, text} = res.recipe
 
             // title
             const editTitle = document.querySelector('#editTitle')
-            editTitle.value = theRecipe.title
+            editTitle.value = title
 
             // type
             const editType = document.querySelector('#editType')
             Object.values(editType.children).forEach((each) => {
-                if (each.value === theRecipe.type) {
+                if (each.value === type) {
                     document.querySelector(`#${each.id}`).setAttribute('selected', 'selected')
                 }
             })
@@ -173,18 +144,23 @@ if (recipeID.length > 0) { // hash 있음 (기존 Recipe 편집)
             // serving
             const editServing = document.querySelectorAll('.foodServing')
             Object.values(editServing).forEach((value) => {
-                if (value.value == theRecipe.serving) {
+                if (value.value == serving) {
                     document.querySelector(`#${value.id}`).setAttribute('selected', 'selected')
                 }
             })
 
+            // Ingredients
+            localStorage.setItem('mainIngre', JSON.stringify(mainIngre))
+            localStorage.setItem('subIngre', JSON.stringify(subIngre))
+            theIngres = { mainIngre, subIngre }
+            renderIngre('main')
+            renderIngre('sub')
+
             // text body
             const editBody = document.querySelector('#editBody')
-            editBody.value = theRecipe.text
-
+            editBody.value = text
         })
-
-} else {    // hash 없음 (새 Recipe 작성)
+} else {    // For a new recipe
     theIngres = {
         mainIngre: [{
             name: '',
@@ -199,25 +175,23 @@ if (recipeID.length > 0) { // hash 있음 (기존 Recipe 편집)
     renderIngre('sub')
 }
 
-
+//------------------------------  Buttons   ------------------------------//
 // Main Ingredient 'Add' Button
 document.querySelector('#addMainIngre').addEventListener('click', () => {
-    const ingreId = uuidv4()
     theIngres.mainIngre.push({
-        id: ingreId,
+        _id: uuidv4(),
         name: '',
         amount: ''
     })
     renderIngre('main')
     localStorage.setItem(`mainIngre`, JSON.stringify(theIngres.mainIngre))
-
 })
 
 // Sub Ingredient 'Add' Button
 document.querySelector('#addSubIngre').addEventListener('click', () => {
     const ingreId = uuidv4()
     theIngres.subIngre.push({
-        id: ingreId,
+        _id: ingreId,
         name: '',
         amount: ''
     })
@@ -225,13 +199,7 @@ document.querySelector('#addSubIngre').addEventListener('click', () => {
     localStorage.setItem(`subIngre`, JSON.stringify(theIngres.subIngre))
 })
 
-///////////////// Make error box disappear when any part of the form is clicked.
-document.querySelector('#edit-form').addEventListener('click', () => {
-    const errorBox = document.querySelector('#edit__error-msg')
-    errorBox.innerHTML = ''
-})
-
-///////////////// Button - Save (Create New)
+// 'Save' Button
 document.querySelector('#edit-form').addEventListener('submit', (e) => {
     e.preventDefault()
 
@@ -249,33 +217,21 @@ document.querySelector('#edit-form').addEventListener('submit', (e) => {
             throw new Error('empty title')
         }
 
-        document.querySelectorAll('#mainIngre').forEach((each) => {
-            const name = each.children[0].value.toLowerCase().trim().replace(/ +/g, ' ').split(' ').join('-')
-            const amount = each.children[1].value.toLowerCase().trim().replace(/ +/g, ' ')
-            if (name.length === 0) {
-                throw new Error('empty Ingre')
-            }
-            body.mainIngre.push({
-                name,
-                amount
+        function collectIngres(type) {
+            document.querySelectorAll(`#${type}Ingre`).forEach((each) => {
+                const name = each.children[0].value.toLowerCase().trim().replace(/ +/g, ' ').split(' ').join('-')
+                const amount = each.children[1].value.toLowerCase().trim().replace(/ +/g, ' ')
+                if (name.length === 0) { throw new Error('empty Ingre') }
+                type === 'main' 
+                ? body.mainIngre.push({ name, amount })
+                : body.subIngre.push({ name, amount })
             })
-        })
-
-        document.querySelectorAll('#subIngre').forEach((each) => {
-            const name = each.children[0].value.toLowerCase().trim().replace(/ +/g, ' ').split(' ').join('-')
-            const amount = each.children[1].value.toLowerCase().trim().replace(/ +/g, ' ')
-            if (name.length === 0) {
-                throw new Error('empty Ingre')
-            }
-            body.subIngre.push({
-                name,
-                amount
-            })
-        })
-
-        if (body.text.length === 0) {
-            throw new Error('empty body')
         }
+        collectIngres('main')
+        collectIngres('sub')
+
+
+        if (body.text.length === 0) {throw new Error('empty body')}
 
         if (pageMode === 'createMode') {
             fetch(`http://localhost:3000/recipes/`, {
@@ -287,9 +243,7 @@ document.querySelector('#edit-form').addEventListener('submit', (e) => {
                 body: JSON.stringify(body)
             })
             .then((res) => {
-                console.log(res)
                 location.assign(`/main.html`)
-
             }).catch((e) => {
                 console.log('Hmm fetch failed')
             })
@@ -305,15 +259,11 @@ document.querySelector('#edit-form').addEventListener('submit', (e) => {
             })
                 .then(response => response.json())
                 .then((res) => {
-                    console.log(res)
                     location.assign(`/main.html`)
-
                 }).catch((e) => {
                     console.log('Hmm fetch failed')
                 })
         }
-
-
     } catch (e) {
         if (e.message === 'empty title') {
             handleError('Title is empty.')
@@ -324,7 +274,6 @@ document.querySelector('#edit-form').addEventListener('submit', (e) => {
         } 
     }
 })
-
 
 // 'Delete' Button
 document.querySelector('#edit-delete').addEventListener('click', (e) => {
